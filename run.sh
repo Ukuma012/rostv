0365f328d0d3730e46e05047d85ee04b248338c5
Ukuma012 <ukuma012@gmail.com>
2025-03-16 22:20:02 +0900

    削除した

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ KERNEL=target/riscv64imac-unknown-none-elf/debug/rost_riscv
 
 cargo build
 $QEMU -machine virt -bios default --no-reboot -serial stdio \
+      -device virtio-gpu-device \
       -kernel $KERNEL
